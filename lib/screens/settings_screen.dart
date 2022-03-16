39c720f6d64b2ac4c0ec11eaab108c767c498d55
Manuel Duarte <carlosd99m@gmail.com>
2022-03-16 10:24:00 -0600

    :lipstick: add operate the radio and switch buttons

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:preferences_app/widgets/widgets.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   static const String routeName = 'Setting';
   const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  bool isDarkMode = false;
+  int gender = 1;
+  String name = 'Manuel';
 
   @override
   Widget build(BuildContext context) {
@@ -23,22 +32,31 @@ class SettingScreen extends StatelessWidget {
             const Divider(),
             SwitchListTile.adaptive(
               title: const Text('Dark Mode'),
-              value: true,
-              onChanged: (value) {},
+              value: isDarkMode,
+              onChanged: (value) {
+                isDarkMode = value;
+                setState(() {});
+              },
             ),
             const Divider(),
             RadioListTile<int>(
               title: const Text('Masculino'),
               value: 1,
-              groupValue: 1,
-              onChanged: (value) {},
+              groupValue: gender,
+              onChanged: (value) {
+                gender = value ?? 1;
+                setState(() {});
+              },
             ),
             const Divider(),
             RadioListTile<int>(
               title: const Text('Femenino'),
               value: 2,
-              groupValue: 1,
-              onChanged: (value) {},
+              groupValue: gender,
+              onChanged: (value) {
+                gender = value ?? 2;
+                setState(() {});
+              },
             ),
             const Divider(),
             Padding(
@@ -49,7 +67,10 @@ class SettingScreen extends StatelessWidget {
                   helperText: 'Ingrese su nombre',
                 ),
                 initialValue: 'Manuel Duarte',
-                onChanged: (value) {},
+                onChanged: (value) {
+                  name = value;
+                  setState(() {});
+                },
               ),
             ),
           ],
